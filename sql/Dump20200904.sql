65ab3a93d74e6aa9a08d0dd148ad361c980745b7
matias96321 <65677371+matias96321@users.noreply.github.com>
2020-09-04 13:03:48 -0300

    update-banco_de_dados

--- sql/Dump20200904.sql
+++ sql/Dump20200904.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `ecomeçe` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `ecomeçe`;
+CREATE DATABASE  IF NOT EXISTS `e-commerce` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `e-commerce`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ecomeçe
