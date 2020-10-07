acdc2e1e0840747d4bf68d41eb16f2f99f06a0b5
Vitor Pereira <vitorsaraivapereira1@gmail.com>
2020-10-07 00:05:51 -0300

    Add tabela quantidade

--- configs/DataBases.js
+++ configs/DataBases.js
@@ -4,8 +4,8 @@ const knex = require('knex')({
   client: 'mysql2',
   connection: {
       host : 'localhost',
-      user : 'root',
-      password : '',
+      user : 'vitor',
+      password : '12345',
       database : 'e-commerce'
     }
 })
