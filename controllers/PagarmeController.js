9d3605471890038a2054ad43f3f890c78651cd9e
Patrick Moreira <ptkdev@outlook.com>
2020-11-02 12:06:12 -0300

    :rocket: Correção de rotas com o front, possivelmente todas as rotas já estão prontas e funcionando. (realizar testes para ter certeza)

--- controllers/PagarmeController.js
+++ controllers/PagarmeController.js
@@ -1,17 +1,9 @@
-const pagarme = require('pagarme');
-const {request,response} = require('express')
+const pagarme = require("pagarme");
 
-//https://docs.pagar.me/docs/obtendo-os-dados-do-cartao
-/*pagarme.client.connect({ encryption_key: 'ek_test_i6vE2KF2jaGTbObC6nH768kVAglUNN' })
-          .then(client => client.security.encrypt(card))
-          .then(card_hash => console.log(card_hash))
-
-*/
 //Consulta de saldo
 
 module.exports = {
-
-  async Total(Request = request,Response = response){
+  async Total(req, res) {
     pagarme.client
       .connect({ api_key: "ak_test_moyHJWO5yY9VUWgPvzHg5RAHR5uNn0" })
       .then((client) => client.balance.primary())
@@ -20,67 +12,76 @@ module.exports = {
       });
   },
 
-  async Sacar(Request = request,Response = response){
-      pagarme.client
-        .connect({ api_key: "ak_test_moyHJWO5yY9VUWgPvzHg5RAHR5uNn0" })
-        .then((client) =>
-          client.transfers.create({
-            amount: 10000,
-            bank_account_id: "id_conta_do_banco",
-            recipient_id: "Recebedor_id",
-          })
-        )
-        .then((transfer) => {
-          return res.send(transfer);
-        });
+  async Sacar(req, res) {
+    pagarme.client
+      .connect({ api_key: "ak_test_moyHJWO5yY9VUWgPvzHg5RAHR5uNn0" })
+      .then((client) =>
+        client.transfers.create({
+          amount: 10000,
+          bank_account_id: "id_conta_do_banco",
+          recipient_id: "Recebedor_id",
+        })
+      )
+      .then((transfer) => {
+        return res.send(transfer);
+      });
   },
 
-  async PagamentoCartao(Request, Response) {
-    const { valorTotal, nomeCartao, dataExpiracao, numeroCartao, cvv, costumer, items } = Request.body;
+  async PagamentoCartao(req, res) {
+    //Resgatando o que vem do front-end
+    const {
+      amount,
+      card_holder_name,
+      card_expiration_date,
+      card_number,
+      card_cvv,
+      customer,
+      shipping,
+      billing,
+      items,
+    } = req.body;
 
-    pagarme.client.connect({ api_key: 'ak_test_moyHJWO5yY9VUWgPvzHg5RAHR5uNn0' })
-    .then(client => client.transactions.create({
-      "amount": valorTotal,
-      "card_number": numeroCartao,
-      "card_cvv": cvv,
-      "card_expiration_date": dataExpiracao,
-      "card_holder_name": nomeCartao,
-      "customer": costumer,
-      "billing": {
-        "name": "Trinity Moss",
-        "address": {
-          "country": "br",
-          "state": "sp",
-          "city": "Cotia",
-          "neighborhood": "Rio Cotia",
-          "street": "Rua Matrix",
-          "street_number": "9999",
-          "zipcode": "06714360"
-        }
-      },
-      "shipping": {
-        "name": "Neo Reeves",
-        "fee": 1000,
-        "delivery_date": "2000-12-21",
-        "expedited": true,
-        "address": {
-          "country": "br",
-          "state": "sp",
-          "city": "Cotia",
-          "neighborhood": "Rio Cotia",
-          "street": "Rua Matrix",
-          "street_number": "9999",
-          "zipcode": "06714360"
-        }
-      },
-      "items": items
-    }))
-    .then((transaction) => {
-      return res.json({transaction: transaction})
-    })
+    pagarme.client
+      .connect({ api_key: "ak_test_moyHJWO5yY9VUWgPvzHg5RAHR5uNn0" })
+      .then((client) =>
+        client.transactions.create({
+          amount: amount,
+          card_number: card_number,
+          card_cvv: card_cvv,
+          card_expiration_date: card_expiration_date,
+          card_holder_name: card_holder_name,
+          customer: customer,
+          billing: billing,
+          shipping: shipping,
+          items: items,
+        })
+      )
+      .then((transaction) => {
+        return res.json({ transaction: transaction });
+      })
+      .catch((err) => {
+        return res.json({ message: err.response.error });
+      });
+  },
 
-  }
-
-  }
-
-
+  async PagamentoBoleto(req, res) {
+    const { amount, payment_method, postback_url, costumer } = req.body;
+    pagarme.client
+      .connect({ api_key: "ak_test_moyHJWO5yY9VUWgPvzHg5RAHR5uNn0" })
+      .then((client) =>
+        client.transactions.create({
+          amount: amount,
+          payment_method: payment_method,
+          postback_url: postback_url,
+          customer: costumer,
+        })
+      )
+      .then((transaction) => {
+        return res.json({ transaction: transaction });
+      })
+      .catch((err) => {
+        console.log(err.response);
+        return res.json({ message: err.response.error });
+      });
+  },
+};
